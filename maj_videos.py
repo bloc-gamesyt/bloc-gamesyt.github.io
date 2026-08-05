bab3e071783f3a61d27862e37540c6f6b6b64e98
bloc-gamesyt <crazygamespc001@gmail.com>
2026-08-06 00:12:48 +0200

    Update maj_videos.py

--- maj_videos.py
+++ maj_videos.py
@@ -1,22 +1,44 @@
 from yt_dlp import YoutubeDL
 import json
 
-CHANNEL = "https://www.youtube.com/@Bloc-gamesYT/videos"
+CHANNEL_URL = "https://www.youtube.com/@Bloc-gamesYT/videos"
 
-opts = {
-    "extract_flat": True,
-    "quiet": True
+options = {
+    "extract_flat": False,
+    "quiet": True,
+    "playlistend": 50
 }
 
-with YoutubeDL(opts) as ydl:
-    data = ydl.extract_info(CHANNEL, download=False)
+with YoutubeDL(options) as ydl:
+    infos = ydl.extract_info(CHANNEL_URL, download=False)
 
 videos = []
 
-for video in data["entries"]:
-    videos.append(video["id"])
+for video in infos["entries"]:
 
-with open("videos.json","w",encoding="utf-8") as f:
-    json.dump(videos,f,indent=4)
+    videos.append({
+        "id": video.get("id"),
+        "titre": video.get("title"),
+        "vues": video.get("view_count", 0),
+        "date": video.get("upload_date"),
+        "miniature": video.get("thumbnail")
+    })
 
-print("videos.json mis à jour")
+
+# Tri du plus récent au plus ancien
+videos.sort(
+    key=lambda x: x["date"] if x["date"] else "",
+    reverse=True
+)
+
+
+with open("videos.json", "w", encoding="utf-8") as fichier:
+    json.dump(
+        videos,
+        fichier,
+        indent=4,
+        ensure_ascii=False
+    )
+
+
+print(f"{len(videos)} vidéos enregistrées !")
